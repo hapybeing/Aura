7a56d1619c19c1af61b2b0693b9a0d94c5f35421
hapybeing <gaurangk.inbox@gmail.com>
2026-03-20 15:03:15 +0530

    Update scroll-store.ts

--- lib/scroll-store.ts
+++ lib/scroll-store.ts
@@ -6,11 +6,13 @@ interface ScrollState {
   scrollVelocity: number
   scrollDirection: number
   manifestoProgress: number
+  collectiveProgress: number
   setScrollY: (y: number) => void
   setScrollProgress: (progress: number) => void
   setScrollVelocity: (velocity: number) => void
   setScrollDirection: (direction: number) => void
   setManifestoProgress: (v: number) => void
+  setCollectiveProgress: (v: number) => void
 }
 
 export const useScrollStore = create<ScrollState>()((set) => ({
@@ -19,9 +21,11 @@ export const useScrollStore = create<ScrollState>()((set) => ({
   scrollVelocity: 0,
   scrollDirection: 0,
   manifestoProgress: 0,
+  collectiveProgress: 0,
   setScrollY: (y) => set({ scrollY: y }),
   setScrollProgress: (progress) => set({ scrollProgress: progress }),
   setScrollVelocity: (velocity) => set({ scrollVelocity: velocity }),
   setScrollDirection: (direction) => set({ scrollDirection: direction }),
   setManifestoProgress: (v) => set({ manifestoProgress: v }),
+  setCollectiveProgress: (v) => set({ collectiveProgress: v }),
 }))
