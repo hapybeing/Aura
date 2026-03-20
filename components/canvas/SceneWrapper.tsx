edc4af6aac80b45fa29de1984b46cf5250979a50
hapybeing <gaurangk.inbox@gmail.com>
2026-03-20 14:43:18 +0530

    Create SceneWrapper.tsx

---
+++ components/canvas/SceneWrapper.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Canvas } from '@react-three/fiber'
+import { Preload } from '@react-three/drei'
+import { Suspense } from 'react'
+import AuraBlob from './AuraBlob'
+import SceneManager from '../core/SceneManager'
+
+export default function SceneWrapper() {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        inset: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 0,
+        pointerEvents: 'none',
+      }}
+    >
+      <Canvas
+        dpr={[1, 1.5]}
+        camera={{ position: [0, 0, 5], fov: 45 }}
+        gl={{
+          antialias: true,
+          alpha: true,
+          powerPreference: 'high-performance',
+        }}
+        style={{ background: 'transparent' }}
+      >
+        <Suspense fallback={null}>
+          <SceneManager />
+          <AuraBlob />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
+  )
+}
